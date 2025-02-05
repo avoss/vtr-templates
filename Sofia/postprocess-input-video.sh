d0d3ee2ae7fe19c34ac38406bfca78ae29947c68
Andreas Voss <mail@a-voss.de>
2025-02-05 21:35:36 +0000

    Use image after 1 second as thumbnail for Sofia

--- Sofia/postprocess-input-video.sh
+++ Sofia/postprocess-input-video.sh
@@ -1,2 +1,2 @@
 # extract 1st frame of video as thumbnail image
-ffmpeg -loglevel warning -y -i video.mp4 -vf "scale=1280:720" -frames:v 1 thumbnail.png
+ffmpeg -loglevel warning -y -ss 30 -i video.mp4 -vf "scale=1280:720" -frames:v 1 thumbnail.png
